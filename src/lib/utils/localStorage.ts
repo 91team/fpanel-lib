dff3efdf14e2e9b5f1973ebbec3061d8402357dd
EvgeniyBV <evg.ml.1995@gmail.com>
2022-05-19 01:41:06 +0300

    refactor: removed unused  code

--- src/lib/utils/localStorage.ts
+++
@@ -1,5 +0,0 @@
-export const setDataToLS = (key: string, value: any) => {
-  localStorage.setItem(key, JSON.stringify(value))
-}
-
-export const getDataFromLS = (key: string) => JSON.parse(localStorage.getItem(key)!)
